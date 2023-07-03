99fd96e85f9a9f4dfbffc6f70865b22e93efd263
Oseeva <fselti1168rus@gmail.com>
2023-07-03 13:45:29 +0700

    начальный коммит

---
+++ src/Book.java
@@ -0,0 +1,10 @@
+public class Book {
+    String name;
+    int yearOfRelease;
+    Author Author;
+
+    public Book(String name, int yearOfRelease) {
+        this.name = name;
+        this.yearOfRelease = yearOfRelease;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    Book book1 = new Book("Harry Potter", 1997);
+        System.out.println("book1.name = " + book1.name);
+        System.out.println("book1.yearOfRelease = " + book1.yearOfRelease);
+
+    Book book2 = new Book("The Secret History", 1992);
+        System.out.println("book2.name = " + book2.name);
+        System.out.println("book2.yearOfRelease = " + book2.yearOfRelease);
     }
+
 
 }
